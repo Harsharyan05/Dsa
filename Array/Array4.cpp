31f07b08f94ed732241b9af31c79bd948b1df2c4
HARSH ARYAN <ha5802@srmist.edu.in>
2026-07-03 20:03:08 +0530

    Add files via upload

---
+++ Array/Array4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr [100];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int mini = arr[0];
+    
+    for(int i = 1; i < n; i++){
+        if(arr[i] < mini)
+        mini = arr[i];
+    
+    }
+
+    cout << mini;
+
+    return 0;
+}

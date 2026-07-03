480ee1037b1d0908024f216f83249cfeb92903cd
HARSH ARYAN <ha5802@srmist.edu.in>
2026-07-03 20:50:01 +0530

    Add files via upload

---
+++ Array/Array10.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++) 
+    cin >> arr[i];
+
+bool sorted = true;
+
+for(int i = 0; i < n-1; i++) {
+
+    if(arr[i] > arr[i+1]) {
+
+        sorted = false;
+        break;
+    }
+}
+
+if(sorted)
+    cout << "Sorted";
+
+else
+    cout << "Not Sorted";
+    
+return 0;    
+
+}    

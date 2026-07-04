4b9bf44b39ff9cedfebc29521ed31f64e8770a30
HARSH ARYAN <ha5802@srmist.edu.in>
2026-07-04 21:11:01 +0530

    Add files via upload

---
+++ Array/Array18.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n; 
+
+    int arr[100];
+
+    int pos = 0;
+    int neg = 0;
+    int zero = 0;
+
+    for(int i = 0; i < n; i++)
+    cin >> arr[i];
+
+    for(int i = 0; i < n; i++){
+
+        if(arr[i] > 0)
+        pos++;
+
+        else if(arr[i] < 0)
+        neg++;
+
+        else
+        zero++;
+    }
+
+    cout << "Positive = " << pos << endl;
+    cout << "Negative = " << neg << endl;
+    cout << "Zero = " << zero;
+
+    return 0;
+}

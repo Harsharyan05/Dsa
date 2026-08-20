bc5279c848fbcfe51b24b34481b53ca3a9f61b89
HARSH ARYAN <aryanharsh03@gmail.com>
2026-08-21 02:21:24 +0530

    Add new DSA problems

---
+++ Revision/Armstrong number.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int original = n;
+    int sum = 0;
+
+    while(n > 0) {
+        int digit = n % 10;
+        sum = sum + (digit * digit * digit);
+        n = n / 10;
+    }
+
+    if (original == sum) {
+        cout << "Armstrong Number";
+    }
+    else {
+        cout << "Not an Armstrong Number";
+    }
+    return 0;
+}

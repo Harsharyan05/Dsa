39e20682004815efa81c40cd96311ca508f873ad
HARSH ARYAN <aryanharsh03@gmail.com>
2026-08-20 23:49:27 +0530

    Add new DSA problems

---
+++ Revision/if-else4.cpp
@@ -0,0 +1,27 @@
+// Electricity Bill with Slab
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int units;
+    cin >> units;
+
+    int bill;
+
+    if (units <= 100) {
+        bill = units * 5;
+    }
+    else if (units <= 200) {
+        bill = 100 * 5 + (units - 100) * 7;
+    }
+    else if (units <= 300) {
+        bill = 100 * 5 + 100 * 7 + (units - 200) * 10;
+    }
+    else {
+        bill = 100 * 5 + 100 * 7 + 100 * 10 + (units - 300) * 15; 
+    }
+    cout << bill;
+    return 0;
+}

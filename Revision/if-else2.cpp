c9de4ce9d3462038ca191a9683653ef313004696
HARSH ARYAN <aryanharsh03@gmail.com>
2026-08-20 23:36:51 +0530

    Add new DSA problems

---
+++ Revision/if-else2.cpp
@@ -0,0 +1,21 @@
+// Check Whether a year is a leap year or not
+#include <iostream>
+using namespace std;
+int main() {
+    int year;
+    cin >> year;
+
+    if (year % 400 == 0) {
+        cout << "Leap Year";
+    }
+    else if (year % 100 == 0) {
+        cout << "Not a Leap Year";
+    }
+    else if (year % 4 == 0) {
+        cout << "Leap Year";
+    }
+    else {
+        cout << "Not a Leap Year";
+    }
+    return 0;
+}

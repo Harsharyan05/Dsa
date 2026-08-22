e09521e34b32aed5fe1c521dfc4a9108376215d3
HARSH ARYAN <aryanharsh03@gmail.com>
2026-08-23 01:14:49 +0530

    Maximum and minimum Classifications of numbers

---
+++ Revision/max_mini.cpp
@@ -0,0 +1,30 @@
+//Find Maximum and Minimum
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++)
+    cin >> arr[i];
+
+    int maxi = arr[0];
+    int mini = arr[0];
+
+    for(int i = 1; i < n; i++){
+
+        if(arr[i] > maxi)
+            maxi = arr[i];
+        if(arr[i] < mini)
+            mini = arr[i];
+    }
+
+    cout << "Maximum = " << maxi << endl;
+    cout << "Minimum = " << mini;
+
+    return 0;
+}

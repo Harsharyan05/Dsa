8eddf51361e67a173a043bbbdaff295a1bc74c02
HARSH ARYAN <aryanharsh03@gmail.com>
2026-08-23 03:49:34 +0530

    Add Unique_no

---
+++ Revision/Unique_no.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        int x;
+        cin >> x;
+
+        ans ^= x;
+    }
+
+    cout << "Unique = " << ans;
+
+    return 0;
+}

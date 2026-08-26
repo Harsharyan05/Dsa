5917faa3ba628214b8c947d785aff0e51a902b0f
HARSH ARYAN <aryanharsh03@gmail.com>
2026-08-27 01:13:16 +0530

    Moore voting algorithm

---
+++ Array/Adv_array/Moore_Voting_Algo.cpp
@@ -0,0 +1,32 @@
+// Moore Voting Algorithm
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++)
+    cin >> arr[i];
+
+    int candidate = arr[0];
+    int count = 1;
+
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] == candidate)
+            count++;
+        else
+            count--;
+        if (count == 0) {
+            candidate = arr[i];
+            count = 1;
+        }    
+    }
+
+    cout << candidate;
+    return 0;
+}
